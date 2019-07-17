5178bc845dfac6f3b2136234a60f6bb46207b6be
matkaingo <50921004+matkaingo@users.noreply.github.com>
2019-07-17 11:34:34 -0500

    Add files via upload

---
+++ Solucion T_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primo test = new Primo();
+            int num = 388;
+
+            int primo = test.Num(num);
+
+            Console.WriteLine(primo);
+            Console.Read();
+
+        }
+    }
+}
