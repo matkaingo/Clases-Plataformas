41b6331a586eb31c6df5c0638b270ee59803a0c8
matkaingo <50921004+matkaingo@users.noreply.github.com>
2019-07-21 10:16:16 -0500

    Add files via upload

---
+++ Solucion T_1/Par-Impar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plataformas_1
+{
+    public class Par_Impar
+    {
+        public string Clasifi(int num)
+        {
+            int numo = num;
+            string clasi = "";
+                if (numo%2==0)
+                {
+                    clasi = "Es par";
+                }
+            else
+            {
+                clasi = "Es impar";
+
+            }
+            return clasi;
+        }
+              
+    }
+   
+}
