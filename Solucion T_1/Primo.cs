fb73e45c96984e57b949d37a5741639f29d0234c
matkaingo <50921004+matkaingo@users.noreply.github.com>
2019-07-21 10:16:57 -0500

    Add files via upload

---
+++ Solucion T_1/Primo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plataformas_1
+{
+
+        public class Primo
+        {
+
+            public int Num(int numero)
+            {
+                int divisor = 2;
+                int con = 0;
+                int numeros = 0;
+                while (con < numero)
+                {
+                    for (int i = 1; i <= divisor; i++)
+                    {
+                        if (divisor % i == 0)
+                        {
+                            numeros++;
+                        }
+                    }
+                    if (numeros <= 2)
+                    {
+                        con++;
+                    }
+                    divisor++;
+                    numeros = 0;
+                }
+
+
+                return divisor - 1;
+            }
+        }
+    }
+
