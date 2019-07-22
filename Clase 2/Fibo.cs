36b09db39f6f6b94da8093e351052367f309a1ec
matkaingo <50921004+matkaingo@users.noreply.github.com>
2019-07-22 06:37:43 -0500

    Add files via upload

---
+++ Clase 2/Fibo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SucesionFibo
+{
+    class Fibo
+    {
+        public int Secuencia(int fib)
+        {
+            int l = fib;
+            int i = 0;
+            int[] fibo = new int[l];
+            int result;
+            do
+            {
+                if (i == 0)
+                {
+                    fibo[i] = 0;
+                }
+                else if (i == 1)
+                {
+                    fibo[i] = 1;
+                }
+                else
+                {
+                    fibo[i] = fibo[i - 1] + fibo[i - 2];
+                }
+
+                result = fibo[i];
+                i++;
+            } while (i<l);
+
+            return result;
+        }
+    }
+}
---
+++ Clase 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SucesionFibo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibo suce = new Fibo();
+
+            int num = suce.Secuencia(12);
+
+            Console.WriteLine("El # es: "+num);
+            Console.Read();
+        }
+    }
+}
