cf97d345bc20aeaca33ba6ae9c66db8c987a424f
matkaingo <50921004+matkaingo@users.noreply.github.com>
2019-07-21 19:31:53 -0500

    Add files via upload

---
+++ Solucion T_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plataformas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primo primo = new Primo();
+            Par_Impar clasifi = new Par_Impar();
+            Romano roman = new Romano();
+
+            int num = 388;
+
+            int numP = primo.Num(num);
+            string clasifica = clasifi.Clasifi(num);
+            string romano = roman.Conversor(numP);
+
+            Console.WriteLine("\nRepaso POO, Plataformas empresariales \n-------------------------------");
+            Console.WriteLine("Primo:\nEl # primo 388 es:\n "+numP);
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Par/Impar de "+numP+":\n "+clasifica);
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine(numP+" en Romano:\n " +romano);
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("En letra:\n");
+            Console.WriteLine("---------------------------------");
+
+            Console.Read();
+        }
+    }
+}
