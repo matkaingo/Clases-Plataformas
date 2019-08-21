2f69b580a7df72f995e3bdc7325b390789f0e94d
matkaingo <50921004+matkaingo@users.noreply.github.com>
2019-08-21 05:39:01 -0500

    Add files via upload

---
+++ Silabas/separador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silabas
+{
+    class separador
+    {
+        private string s;
+        private string separated = ""; //Almacena como va quedadno la palabra separada
+        public void separa(string a)
+        {
+            s = a;
+            String[] palabras = s.Split(new string[] { " " }, StringSplitOptions.None);
+            Divisor divisor = new Divisor();
+
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                divisor.setString((String)palabras[i]);
+                separated = separated + " " + divisor.silabear();
+            }
+            Console.WriteLine(separated);
+            Console.Read();
+        }
+    }
+}
